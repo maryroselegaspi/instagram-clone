4ae4002b2268752e8238a631e7ee52975c6b5f73
maryrose <maryrose.legasp2018@gmail.com>
2020-09-22 14:48:54 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Post from './Post';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const [post, setPost] = useState([
+  {
+    username:'clever Mary' ,
+    caption:'Great coding' ,
+    imageUrl:'https://images.freeimages.com/images/large-previews/256/spring-1376144.jpg',
+  },
+  {
+    username:'Mary' ,
+    caption:'Nice work' ,
+    imageUrl:'https://images.freeimages.com/images/large-previews/48f/spring-colour-1399971.jpg',
+  },
+  {
+    username:'clever' ,
+    caption:'Making more projects' ,
+    imageUrl:'https://images.freeimages.com/images/large-previews/fa6/spring-1376376.jpg',
+  },
+]);
+
+  return(
+    <div className="app">
+      {/* Header */}
+      <div className='app__header'>
+        <img className='app__headerImage'
+              src='https://hd-report.com/wp-content/uploads/2016/02/instagram_text_logo_blk-600x228.png'
+              alt=''
+        />
+      </div>
+      <h1>Hello there! This is Instagram cloning project</h1> 
+
+      {/* Posts */}
+      {
+        post.map( post =>(
+          <Post 
+            username={post.username} 
+            caption={post.caption} 
+            imageUrl={post.imageUrl}      
+          />
+        ))
+      }
     </div>
   );
 }
